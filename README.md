87f7d90d4255dd344f0f3fed6075605d3af0fef8
challita <challita@kth.se>
2021-04-12 20:06:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# brodboden_backend
---
+++ brodboden_backend/src/test/java/brodbodenTeam/brodboden_backend/BrodbodenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package brodbodenTeam.brodboden_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrodbodenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
