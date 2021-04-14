9576450f1ddb33f5ec043622420467fd327f8802
challita <challita@kth.se>
2021-04-15 00:24:47 +0200

    fixed tables + database

---
+++ brodboden_backend/src/main/java/brodbodenTeam/brodboden_backend/repositories/DrinkRepository.java
@@ -0,0 +1,8 @@
+package brodbodenTeam.brodboden_backend.repositories;
+
+import brodbodenTeam.brodboden_backend.objects.Drink;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DrinkRepository extends CrudRepository<Drink, Integer> {
+    //Functions for later
+}
